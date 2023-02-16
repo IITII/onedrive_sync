3ed1a600afe5a164fa63254f86013b04900188b7
IITII <ccmejx@gmail.com>
2023-02-16 22:09:55 +0800

    deprecate

--- config/config.js
+++ config/config.js
@@ -7,7 +7,9 @@ const fs = require('fs'),
   https = require('https'),
   path = require('path')
 const opts = {
-  origin: process.env.SYNC_ORIGIN || '',
+  origin: process.env.SYNC_ORIGIN || 'https://63v7q8-my.sharepoint.com/personal/hope773715568_63v7q8_onmicrosoft_com/_api/web/GetListUsingPath(DecodedUrl=@a1)/RenderListDataAsStream?@a1=%27%2Fpersonal%2Fhope773715568%5F63v7q8%5Fonmicrosoft%5Fcom%2FDocuments%27&RootFolder=%2Fpersonal%2Fhope773715568%5F63v7q8%5Fonmicrosoft%5Fcom%2FDocuments%2F%E5%86%99%E7%9C%9F&TryNewExperienceSingle=TRUE',
+  cookie: process.env.SYNC_COOKIE || 'FedAuth=77u/PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0idXRmLTgiPz48U1A+VjEzLDBoLmZ8bWVtYmVyc2hpcHx1cm4lM2FzcG8lM2Fhbm9uI2NmMTA4Y2E5ZTNmYmQ3MGI4OWMwMzlhZWE0MzcyNGE0NWVhYjVmMDE2MTkzODM0ZjQzYzNmZTQwNWRiNzUwYTAsMCMuZnxtZW1iZXJzaGlwfHVybiUzYXNwbyUzYWFub24jY2YxMDhjYTllM2ZiZDcwYjg5YzAzOWFlYTQzNzI0YTQ1ZWFiNWYwMTYxOTM4MzRmNDNjM2ZlNDA1ZGI3NTBhMCwxMzMyMTAyNTQ3MTAwMDAwMDAsMCwxMzMyMTExMTU3MTM3MzQ4MjUsMC4wLjAuMCwyNTgsZTc1ODBlNmEtODMxNy00YTExLTliYTctYjQyMDM2MWZmOTZhLCwsZjljNjk2YTAtMTBmMS0zMDAwLTNiYzMtYzhhYmZlYjhiMTIzLGY5YzY5NmEwLTEwZjEtMzAwMC0zYmMzLWM4YWJmZWI4YjEyMyxlMFI5eEY0NnMwR1Mxb0VqN1hubFR3LDAsMCwwLCwsLDI2NTA0Njc3NDM5OTk5OTk5OTksMCwsLCwsLCwwLCxVU18yMTNfQ29udGVudCxEYS9uWEZhVHQ4V1pqK3BNNHRCVlZkc3o5NHBaLzM0d0d3MTJWVWl6dStFOGQzRFd0TkE0SzkrRVNwcDV3WldjQ0hXeHNBV0kyZkdVRVA2NDB4NzI2UFZFYWN2RGFLUm0wRXZmOFpDVWJSTW9ZSmY2YmhNVnprbE9rRUg3VlNzQXdrNy83UGlPUVR4ZnZ4VXdZSEtTZFo2VHJtTmxpVkZXdS9sUEhPa1h6eTFFaTl5MTZPSXZLQkIwVWJCWWUybjFzMlpOamd1d29kS1NVNEJ6MnhFSzIzN2o2VWFIbS9lRndtd3dWeWFxNDIwUi9zVlVhaUh1SGl6QW1oWm0wTTNHSWg2aFZES0xwMDYrWWZwMmF1TUlYamJmRndnWjVQVGtlTUR4V3JIdWxWQUQzb2VYc0RJbUtZYlpZQjJyMnNvTWpDQjlWZzEra3VIR2JDTTRHcVg2UkE9PTwvU1A+',
+  downloadUrl: process.env.SYNC_DOWNLOAD || 'https://63v7q8-my.sharepoint.com/personal/hope773715568_63v7q8_onmicrosoft_com/_layouts/15/download.aspx?UniqueId=6FF8216B-33F9-4C7E-AC58-8CB10B1F8EAD',
   sync: {
     // 本地路径
     local: process.env.SYNC_LOCAL || '../tmp',
@@ -33,6 +35,9 @@ opts.api = {
   opts: {
     responseType: 'json',
   },
+  headers: {
+    'Cookie': opts.cookie,
+  }
 }
 
 // DO NOT TOUCH UNLESS YOU KNOW WHAT YOU ARE DOING
@@ -47,6 +52,7 @@ const config = {
     timeout: 1000 * 20,
     headers: {
       'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36',
+      ...opts.api.headers,
     },
     httpsAgent: new https.Agent({
       rejectUnauthorized: false,
@@ -59,10 +65,11 @@ const config = {
 if (!config.origin) {
   throw new Error('SYNC_ORIGIN is required')
 }
-if (!config.regex.test(config.origin)) {
-  throw new Error('SYNC_ORIGIN is invalid')
-}
-config.searchParams = new URL( config.origin).searchParams
+// if (!config.regex.test(config.origin)) {
+//   throw new Error('SYNC_ORIGIN is invalid')
+// }
+config.downloadUrl = new URL(config.downloadUrl)
+config.searchParams = new URL(config.origin).searchParams
 config.sync.local = path.resolve(__dirname, config.sync.local)
 // config.cache_file = path.resolve(__dirname, config.cache_file)
 // config.cache_file_bak = `${config.cache_file}.bak`
--- libs/api.utils.js
+++ libs/api.utils.js
@@ -6,45 +6,64 @@
 
 const path = require('path')
 const {axios} = require('./axios_client.js')
-const cloudscraper = require('cloudscraper')
-const {api} = require('../config/config.js')
+const {api, origin, cookie, downloadUrl} = require('../config/config.js')
+let cloudscraper = require('cloudscraper')
 const {logger} = require('./logger.js')
 const {titleFormat} = require('./utils.lib.js')
 
-async function api_get(url, params = {}) {
+cloudscraper = cloudscraper.defaults({headers: {...cloudscraper.defaultParams.headers, ...api.headers}})
+
+// api_get(origin, {}, 'POST').then(_ => {
+api_get('https://63v7q8-my.sharepoint.com/personal/hope773715568_63v7q8_onmicrosoft_com/_api/web/GetListUsingPath(DecodedUrl=@a1)/RenderListDataAsStream?@a1=\'/personal/hope773715568_63v7q8_onmicrosoft_com/Documents\'&RootFolder=/personal/hope773715568_63v7q8_onmicrosoft_com/Documents/写真/六味帝皇酱/六味帝皇酱 - 脱在分手后&TryNewExperienceSingle=TRUE', {}, 'POST').then(_ => {
+  console.log(_)
+}).catch(e =>{
+  console.log(e)
+})
+
+async function api_get(url, params = {}, method = 'get') {
   let res, full_url
   full_url = new URL(url, api.base).toString()
   if (api.cf) {
     res = cloudscraper({
+      method,
       uri: full_url,
       formData: params,
     }).then(_ => JSON.parse(_))
   } else {
-    res = axios.get(full_url, {...api.opts, params})
-      .then(res => res.data)
+    res = method === 'get' ? axios.get(full_url, {...api.opts, params})
+      : axios.post(full_url, params, api.opts)
+    res = res.then(res => res.data)
   }
   return await res
 }
 
 async function list_dir(tar_url, res = []) {
   let url, params, tmp, count, values, nextLink
-  url = new URL(tar_url)
-  url.searchParams.set('$top', '1000')
-  url = url.toString()
-  tmp = await api_get(url, {})
-  count = tmp['@odata.count']
-  values = tmp.value
-  nextLink = tmp['@odata.nextLink']
+  url = tar_url
+  tmp = await api_get(url, {}, 'POST')
+  // count = tmp['@odata.count']
+  values = tmp['Row']
+  nextLink = tmp['NextHref']
+  let {FirstRow, LastRow} = tmp
   res = res.concat(values)
   if (nextLink) {
-    logger.info(`list dir: ${res.length}/${count}, nextLink: ${nextLink}`)
+    nextLink = nextLink.startsWith('?') ? tar_url + nextLink : nextLink
+    logger.info(`cur: ${FirstRow} -> ${LastRow}, nextLink: ${nextLink}`)
     return await list_dir(nextLink, res)
   } else {
     res = res.map(_ => {
-      let {id, name, size} = _
-      let parseName, downloadUrl, file, mime, folder, pid, link
-      parseName = path.parse(name)
-      downloadUrl = _['@content.downloadUrl']
+      let {UniqueId, FileRef, FileSizeDisplay, FSObjType} = _
+      let FileLeafRefName, FileLeafRefSuffix
+      FileLeafRefName = tmp['FileLeafRef.Name']
+      FileLeafRefSuffix = tmp['FileLeafRef.Suffix']
+
+      let id, name, folder, downloadUrl, link
+
+      downloadUrl.searchParams.set('UniqueId', UniqueId)
+      downloadUrl = downloadUrl.toString()
+      // let parseName, downloadUrl, file, mime, folder, pid, link
+      // parseName = path.parse(name)
+      // downloadUrl = _['@content.downloadUrl']
       file = !!_.file
       folder = !!_.folder
       mime = _?.file?.mimeType
